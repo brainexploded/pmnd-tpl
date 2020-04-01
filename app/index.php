f0d9420b76c1fbd6052dd94d2d5c0d8943cb94aa
bxp <brainexploded@gmail.com>
2020-04-02 00:34:19 +0300

    .env

--- app/index.php
+++ app/index.php
@@ -1,3 +1,10 @@
-<?php
-
-echo 'ы!';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><?php echo 'Upyachka!'; ?></h1>
+  </body>
+</html>
